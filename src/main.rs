2ff80ddab1d89e94b5e532999f0fdce1a015a661
Bastien de Luca <dev@de-luca.io>
2020-07-18 00:11:53 +0200

    Add color presets

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,7 @@
 #[macro_use]
 extern crate lazy_static;
 
-use actix_web::{
-    App,
-    Error,
-    HttpResponse,
-    HttpServer,
-    middleware,
-    Responder,
-    Result,
-    web,
-};
+use actix_web::{App, Error, HttpResponse, HttpServer, middleware, Responder, Result, web};
 use actix_web::http::header::{
     CACHE_CONTROL,
     CacheControl,
@@ -18,68 +9,139 @@ use actix_web::http::header::{
     ETAG,
     EntityTag,
 };
-use rand::{Rng};
+use rand::{distributions::{Distribution, Standard}, Rng};
 use bit_vec::BitVec;
+use rand::prelude::ThreadRng;
+
 
 const GAP: usize = 10;
-const WIDTH: usize = 500;
+const WIDTH: usize = 650;
 const HEIGHT: usize = 250;
+
 
 lazy_static! {
     static ref COLS: usize = WIDTH / GAP - 1;
-    static ref ROWS: usize = ((HEIGHT / GAP) * 2) - 1;
+    static ref ROWS: usize = HEIGHT / GAP - 2;
+}
+
+
+#[derive(Debug)]
+enum ColorPreset {
+    Chaos,
+    Black,
+    Red,
+    Yellow,
+    Green,
+    Cyan,
+    Blue,
+    Purple,
+}
+
+impl Distribution<ColorPreset> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> ColorPreset {
+        match rng.gen_range(0, 8) {
+            0 => ColorPreset::Black,
+            1 => ColorPreset::Red,
+            2 => ColorPreset::Yellow,
+            3 => ColorPreset::Green,
+            4 => ColorPreset::Cyan,
+            5 => ColorPreset::Blue,
+            6 => ColorPreset::Purple,
+            _ => ColorPreset::Chaos,
+        }
+    }
+}
+
+impl ColorPreset {
+    fn hue_range(&self) -> (i16, i16) {
+        match self {
+            ColorPreset::Chaos => (0, 360),
+            ColorPreset::Black => (0, 0),
+            ColorPreset::Red => (-30, 30),
+            ColorPreset::Yellow => (30, 90),
+            ColorPreset::Green => (90, 150),
+            ColorPreset::Cyan => (150, 210),
+            ColorPreset::Blue => (210, 270),
+            ColorPreset::Purple => (330, 390),
+        }
+    }
+
+    fn saturation_range(&self) -> (i16, i16) {
+        match self {
+            ColorPreset::Black => (0, 0),
+            _ => (0, 100),
+        }
+    }
 }
 
 struct Color {
-    red: i16,
-    green: i16,
-    blue: i16,
+    hue: i16,
+    saturation: i16,
+    lightness: i16,
 }
 
 impl Color {
-    fn random() -> Color {
-        let mut rng = rand::thread_rng();
+    fn random(rng: &mut ThreadRng, preset: &ColorPreset) -> Color {
+        let hue_range = preset.hue_range();
+        let saturation_range = preset.saturation_range();
+
+        let hue = rng.gen_range(hue_range.0, hue_range.1 + 1);
 
         Color {
-            red: rng.gen_range(0, 256),
-            green: rng.gen_range(0, 256),
-            blue: rng.gen_range(0, 256),
+            hue: if hue < 0 { 360 + hue } else { hue },
+            saturation: rng.gen_range(saturation_range.0, saturation_range.1 + 1),
+            lightness: rng.gen_range(0, 101),
         }
     }
-    fn hex(&self) -> String {
-        format!("#{:x}{:x}{:x}", self.red, self.green, self.blue)
+
+    fn hsl(&self) -> String {
+        format!("hsl({},{}%,{}%)", self.hue, self.saturation, self.lightness)
     }
 }
 
 
 async fn index() -> Result<impl Responder, Error> {
     let mut rng = rand::thread_rng();
-    let col_colors: Vec<Color> = (0..*COLS).map(|_| Color::random()).collect();
+    let preset: ColorPreset = rand::random();
+
+    let col_colors: Vec<Color> = (0..*COLS).map(|_| Color::random(&mut rng, &preset)).collect();
 
     let mut svg = format!(
-        "<svg width=\"{}px\" height=\"{}px\" xmlns=\"http://www.w3.org/2000/svg\">",
+        "<svg viewBox=\"0 0 {} {}\" xmlns=\"http://www.w3.org/2000/svg\">",
         WIDTH, HEIGHT,
     );
 
     for row in 0..*ROWS {
-        let row_color = Color::random();
-        let mut bits = BitVec::from_bytes(
+        let row_color = Color::random(&mut rng, &preset);
+        let row_bits = BitVec::from_bytes(
             &rng.gen_range(i64::MIN, i64::MAX).to_be_bytes()
         );
-        bits.truncate(*COLS);
+        let col_bits = BitVec::from_bytes(
+            &rng.gen_range(i64::MIN, i64::MAX).to_be_bytes()
+        );
 
-        let even = row % 2 == 0;
         let mut col = 0;
 
-        for display in bits.iter() {
-            if display {
+        for (display_row, display_col) in row_bits.iter().zip(col_bits.iter()) {
+            if display_row {
                 svg.push_str(format!(
                     "<path d=\"M {} {} L {} {}\" stroke=\"{}\" stroke-width=\"2\" />",
-                    col * GAP + if even { GAP } else { 0 },
-                    row * GAP + if even { 0 } else { GAP },
-                    col * GAP + (GAP * if even { 1 } else { 2 }),
-                    row * GAP + (GAP * if even { 2 } else { 1 }),
-                    if even { col_colors.get(col).unwrap().hex() } else { row_color.hex() },
+                    col * GAP + GAP,
+                    row * GAP,
+                    col * GAP + GAP,
+                    row * GAP + (GAP * 2),
+                    row_color.hsl(),
+                ).as_str());
+            }
+
+            if display_col {
+                svg.push_str(format!(
+                    "<path d=\"M {} {} L {} {}\" stroke=\"{}\" stroke-width=\"2\" />",
+                    col * GAP,
+                    row * GAP + GAP,
+                    col * GAP + (GAP * 2),
+                    row * GAP + GAP,
+                    col_colors.get(col).unwrap().hsl(),
                 ).as_str());
             }
 
